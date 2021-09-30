a554a8bd086583131a23ba0132348debef90216b
Xinyi <xinzwerkz@gmail.com>
2021-09-30 11:35:05 +0800

    Add Message action in ReportController and MessageViewModel, add Report/Message view

---
+++ GettingStarted/Models/MessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace GettingStarted.Models
+{
+    public class MessageViewModel
+    {
+        public string Message { get; set; }
+        public string From { get; set; }
+
+        public MessageViewModel()
+        {
+        }
+    }
+}
