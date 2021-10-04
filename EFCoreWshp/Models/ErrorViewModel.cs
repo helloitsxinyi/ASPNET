8385ac0808b8f0567d5182017f71656076717c89
Xinyi <xinzwerkz@gmail.com>
2021-10-04 10:17:02 +0800

    Add EFCoreWorkshop & EFCore packages

---
+++ EFCoreWshp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EFCoreWshp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
