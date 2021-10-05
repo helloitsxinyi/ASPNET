60f1b8b8399674e1b6cb5731714999ffff201d28
Xinyi <xinzwerkz@gmail.com>
2021-10-05 17:26:02 +0800

    Register DB and add DBContext

---
+++ SearchUsers/DB.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using SearchUsers.Models;
+
+namespace SearchUsers
+{
+    public class DB
+    {
+        private DBContext dbContext;
+
+        public DB(DBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Seed()
+        {
+            SeedPersons();
+        }
+
+        private void SeedPersons()
+        {
+            //List<Person> persons = new List<Person>();
+
+            dbContext.Add(new Person
+            {
+                LastName = "Tan",
+                FirstName = "Jerry",
+                JobTitle = "Software Engineer",
+                YearsExperience = 12
+            });
+
+            dbContext.Add(new Person
+            {
+                LastName = "Wong",
+                FirstName = "Hogan",
+                JobTitle = "Data Scientist",
+                YearsExperience = 5
+            });
+
+            dbContext.Add(new Person
+            {
+                LastName = "Lee",
+                FirstName = "Jean",
+                JobTitle = "Consultant",
+                YearsExperience = 15
+            });
+
+            dbContext.Add(new Person
+            {
+                LastName = "Lai",
+                FirstName = "Kelly",
+                JobTitle = "Software Engineer",
+                YearsExperience = 8
+            });
+
+            dbContext.Add(new Person
+            {
+                LastName = "Tan",
+                FirstName = "John",
+                JobTitle = "Consultant",
+                YearsExperience = 10
+            });
+
+            dbContext.Add(new Person
+            {
+                LastName = "Tan",
+                FirstName = "Kim",
+                JobTitle = "Data Scientist",
+                YearsExperience = 10
+            });
+
+            dbContext.Add(new Person
+            {
+                LastName = "Wong",
+                FirstName = "Larry",
+                JobTitle = "Consultant",
+                YearsExperience = 5
+            });
+
+
+            //ViewData["persons"] = persons;
+
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ SearchUsers/Models/DBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SearchUsers.Models
+{
+
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+             
+        public DbSet<Person> Persons { get; set; }
+   
+    }
+}
