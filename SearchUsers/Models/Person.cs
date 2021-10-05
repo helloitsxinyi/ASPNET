11a85579400c2f18d75f1d8cecfdcd181d1d12d5
Xinyi <xinzwerkz@gmail.com>
2021-10-05 13:29:25 +0800

    Add Person model

---
+++ SearchUsers/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SearchUsers.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public int YearsExperience { get; set; }
+    }
+}
