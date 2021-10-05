9908581ebfa93ef80fbe33dffb59410cb88ecccb
Xinyi <xinzwerkz@gmail.com>
2021-10-05 11:30:15 +0800

    Add SearchUsers project

---
+++ EFCoreWshp/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
+
+//namespace EFCoreWshp.Models
+//{
+//    public class Product
+//    {
+//        public Product()
+//        {
+//        }
+
+//        // primary key
+//        [Key]
+//        public Guid Id { get; set; }
+
+//        // columns
+//        [Required]
+//        [MaxLength(36)]
+//        [Column("colName")]
+//        public string Name { get; set; }
+
+//        // datatype: float
+//        // string can be null, but float & int cannot be null.
+//        // if you really want it to be null, put the question mark
+//        // public float? Price { get; set; }
+//        [Column("colPrice")]
+//        public float Price { get; set; }
+     
+//        // datatype: int
+//        [Column("colQuantity")]
+//        public int Quantity { get; set; }
+
+        
+//    }
+//}
