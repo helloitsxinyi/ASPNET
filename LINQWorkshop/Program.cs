2a72fd81df991d18f2cebe8f171786b089d99ad2
Xinyi <xinzwerkz@gmail.com>
2021-10-07 15:37:39 +0800

    Add ans for Q1

--- LINQWorkshop/Program.cs
+++ LINQWorkshop/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQWorkshop
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {        
+            //Q1
+            IEnumerable<Student> iter =
+                from s in Student.students
+                where s.Age >= 12 && s.Age <= 20
+                select s;
+
+            foreach (Student s in iter)
+            {
+                Console.WriteLine($"{s.Name}, {s.Age}");
+            }
+
+            //Q2
+
+
         }
     }
 }
